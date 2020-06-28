dabd5b898281fbe426ebfe96cfd9d7e2f9bcee6b
Septian Solihin <septian.solihin@btpn.com>
2020-06-28 19:00:12 +0700

    initial commit

---
+++ src/test/java/com/mycompany/ReactCSSbackend/ReactCssBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.ReactCSSbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactCssBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
